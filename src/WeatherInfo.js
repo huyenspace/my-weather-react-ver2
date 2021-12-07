24a591996a55c1ba4f8af74508973a6f9f93bf78
huyenspace <huyen.space99@gmail.com>
2021-12-07 10:52:27 +0700

    Design forecast

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -6,38 +6,36 @@ import "bootstrap/dist/css/bootstrap.css";
 export default function WeatherInfo(props) {
   return (
     <div className="WeatherInfo">
-      <div className="container">
-        <div className="row">
-          <div className="col-4">
-            <div className="row mt-5">
-              <div className="col-4">
-                <div className="float-left">
-                  <WeatherIcon code={props.data.icon} size={60} />
-                </div>
-              </div>
-              <div className="col-8">
-                <span className="temperature">
-                  {Math.round(props.data.temperature)}
-                </span>
-                <span className="unit">°C</span>
+      <div className="row">
+        <div className="col-4">
+          <div className="row mt-5">
+            <div className="col-4">
+              <div className="float-left">
+                <WeatherIcon code={props.data.icon} size={60} />
               </div>
             </div>
+            <div className="col-8">
+              <span className="temperature">
+                {Math.round(props.data.temperature)}
+              </span>
+              <span className="unit">°C</span>
+            </div>
           </div>
-          <div className="col-4">
-            <h1>{props.data.city}</h1>
-            <ul className="currentWeather">
-              <li>
-                <FormatDate date={props.data.date} />
-              </li>
-              <li className="text-capitalize">{props.data.description}</li>
-            </ul>
-          </div>
-          <div className="col-4 inforAdd mt-5">
-            <ul>
-              <li>Humidity: {props.data.humidity}%</li>
-              <li>Wind: {Math.round(props.data.wind)} km/h</li>
-            </ul>
-          </div>
+        </div>
+        <div className="col-4">
+          <h1>{props.data.city}</h1>
+          <ul className="currentWeather">
+            <li>
+              <FormatDate date={props.data.date} />
+            </li>
+            <li className="text-capitalize">{props.data.description}</li>
+          </ul>
+        </div>
+        <div className="col-4 inforAdd mt-5">
+          <ul>
+            <li>Humidity: {props.data.humidity}%</li>
+            <li>Wind: {Math.round(props.data.wind)} km/h</li>
+          </ul>
         </div>
       </div>
     </div>
