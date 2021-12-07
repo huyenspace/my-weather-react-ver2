fa60e0ec626fe0f4a3f88444b84138e443f82dad
huyenspace <huyen.space99@gmail.com>
2021-12-07 08:56:07 +0700

    Import all complete document

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello from App</h1>
+      <div className="container">
+        <Weather defaultCity="Paris" />
+      </div>
+      <p>
+        This project was coded by{" "}
+        <a
+          href="https://github.com/huyenspace/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {" "}
+          Huyen Nguyen
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/huyenspace/weather-react"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {" "}
+          open-sourced on Github
+        </a>{" "}
+        and hosted on{" "}
+        <a
+          href="https://www.netlify.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Netlify
+        </a>{" "}
+      </p>
     </div>
   );
 }
